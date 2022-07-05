c0a3b041395753e48ded844fbea82529b84e8874
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-05 13:15:21 -0700

    updated questions and added switch case statement for each team members

--- index.js
+++ index.js
@@ -3,10 +3,39 @@ const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const generateHtml = require ("./dist");
+const generateHtml = require ("./dist/index.html");
 
 //Collecting the data of manager,engineer and intern and pushing it into an array employee.
 const teamMembers = [];
+//questions array for engineer
+const engineerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter your team managers name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter your employee ID?",
+    },
+    {
+      type: "input",
+      name: "address",
+      message: "Please enter your email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your github username?",
+    },
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Who would you like to add next?",
+      choices: ['Engineer','Intern','None'],
+    }
+  ];
 //questions array for manager
 const managerQuestions = [
   {
@@ -36,86 +65,95 @@ const managerQuestions = [
     choices: ['Engineer','Intern','None'],
   }
 ];
-//questions array for intern
-const internQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter your team managers name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Please enter your employee ID?",
-  },
-  {
-    type: "input",
-    name: "address",
-    message: "Please enter your email address?",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "Please enter your school name?",
-  },
-];
-//questions array for engineer
-const engineerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter your team managers name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Please enter your employee ID?",
-  },
-  {
-    type: "input",
-    name: "address",
-    message: "Please enter your email address?",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Please enter your github username?",
-  },
-];
 function addManager() {
-  inquirer.prompt(managerQuestions).then((response) => {
-    console.log(response);
-    const manager = new Manager(
-      response.name,
-      response.id,
-      response.email,
-      response.office
-    );
-    teamMembers.push(manager);
-  });
-}
+    inquirer.prompt(managerQuestions).then((managerResponse) => {
+      console.log(managerResponse);
+      const manager = new Manager(
+          managerResponse.name,
+          managerResponse.id,
+          managerResponse.email,
+          managerResponse.office
+      );
+      teamMembers.push(manager);
+      switch (managerResponse.addEmployee){
+        case 'Engineer':
+           engineerQuestions();
+           break;
+        case  'Intern':
+            internQuestions();
+            break;
+      }
+    });
+  }
+
 function addEngineer() {
-  inquirer.prompt(engineerQuestions).then((response) => {
-    console.log(response);
+  inquirer.prompt(engineerQuestions).then((engineerResponse) => {
+    console.log(engineerResponse);
     const engineer = new Engineer(
-      response.name,
-      response.id,
-      response.email,
-      response.github
+        engineerResponse.name,
+        engineerResponse.id,
+        engineerResponse.email,
+        engineerResponse.github
     );
     teamMembers.push(engineer);
+    switch (engineerResponse.addEmployee){
+        case 'Engineer':
+           engineerQuestions();
+           break;
+        case  'Intern':
+            internQuestions();
+            break;
+      }
   });
 }
+//questions array for intern
+const internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter your name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter your employee ID?",
+    },
+    {
+      type: "input",
+      name: "address",
+      message: "Please enter your email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter your school name?",
+    },
+    {
+        type: "list",
+        name: "addEmployee",
+        message: "Who would you like to add next?",
+        choices: ['Engineer','Intern','None'],
+      }
+  ];
 function addIntern() {
-  inquirer.prompt(internQuestions).then((response) => {
+  inquirer.prompt(internQuestions).then((internResponse) => {
     console.log(response);
     const intern = new Intern(
-      response.name,
-      response.id,
-      response.email,
-      response.school
+        internResponse.name,
+        internResponse.id,
+        internResponse.email,
+        internResponse.school
     );
     teamMembers.push(intern);
+    switch (internResponse.addEmployee){
+        case 'Engineer':
+           engineerQuestions();
+           break;
+        case  'Intern':
+            internQuestions();
+            break;
+      }
   });
 }
+addManager();
 
