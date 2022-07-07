4913c963a9d73d70ca9461ee83cba7eecd64e85c
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-07 16:49:26 -0700

    wrote tests for manager which includes manager properties and methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,21 +1,22 @@
 const Manager = require("../lib/Manager");
-const manager = new Manager("John", "9087", "johnp@yahoo.com");
+const manager = new Manager("John", "9087", "johnp@yahoo.com",'1234');
 describe("ManagerClass", () => {
   it("test to check if we get the constructor values for the manager object", () => {
-    expect(manager.name).toBe("Asha");
-    expect(manager.id).toBe("1078");
-    expect(employee.email).toBe("chourpagar.asha@gmail.com");
+    expect(manager.name).toBe("John");
+    expect(manager.id).toBe("9087");
+    expect(manager.email).toBe("johnp@yahoo.com");
+    expect(manager.officeNumber).toBe("johnp@yahoo.com");
   });
   it("test to check if we can get name from getName()method", () => {
-    expect(employee.getName()).toBe("Asha");
+    expect(manager.getName()).toBe("John");
   });
   it("test to check if we can get id from getId()method", () => {
-    expect(employee.getId()).toBe("1078");
+    expect(manager.getId()).toBe("9087");
   });
   it("test to check if we can get email address from getEmail()method", () => {
-    expect(employee.getEmail()).toBe("chourpagar.asha@gmail.com");
+    expect(manager.getEmail()).toBe("johnp@yahoo.com");
   });
   it("test to check if we can get role from getRole()method", () => {
-    expect(employee.getRole()).toBe("Employee");
+    expect(manager.getRole()).toBe("Manager");
   });
 });
